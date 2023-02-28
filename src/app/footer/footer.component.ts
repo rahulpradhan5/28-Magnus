59ca84c46a1ecc3a495e2d36c73a187eb20f3e7b
jafrin-iqbal-chowdhury <ishaishrat75@gmail.com>
2023-02-28 22:03:52 +0600

    footer-removed

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../shared/auth.service';
+import { Event, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -8,13 +11,34 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 })
 export class FooterComponent implements OnInit {
   logedIn = false;
-  constructor(public auths:AngularFireAuth) {
-    auths.user.subscribe(data=>{
-      if(data?.uid != null) {
-        this.logedIn = true;
-      }else this.logedIn = false;
-    })
-   }
+  register1 = true;
+  login1 = true;
+  logout1 = false;
+  isLogedin = false;
+  currentRoute: string = "";
+  set: boolean = true;
+
+  constructor(private auths: AuthService, public fire: AngularFireAuth, public store: AngularFirestore, private router: Router, public route: ActivatedRoute) {
+    this.currentRoute = "Demo";
+    this.router.events.subscribe((event: Event) => {
+
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+        console.log(event.url);
+        if (this.currentRoute == '/dashboard' || this.currentRoute == '/generate-E-pin' || this.currentRoute == '/network' || this.currentRoute == '/upgrade' || this.currentRoute == '/my-earnings' || this.currentRoute == '/ComposeMessage'
+          || this.currentRoute == '/tree' || this.currentRoute == '/GenerationTreeView' || this.currentRoute == '/MyDirect' || this.currentRoute == '/MyDownline' || this.currentRoute == '/Idwisedownline' || this.currentRoute == '/GenerationDownline'
+          || this.currentRoute == '/CommisionLedger' || this.currentRoute == '/TDSDetails' || this.currentRoute == '/Inbox' || this.currentRoute == '/Sent' || this.currentRoute == '/Folders' || this.currentRoute == '/CheckEpin' || this.currentRoute == '/MemberEpinLogin' || this.currentRoute == '/EPinChangePassword'
+          || this.currentRoute == '/EPinChangePassword' || this.currentRoute == '/EPinChangePassword' || this.currentRoute == "/RankQualifiersReport" || this.currentRoute == '/LevelReconcilationReport' || this.currentRoute == '/RankQualifiersReport'
+          || this.currentRoute == '/SchemeOfferDetailsReport' || this.currentRoute == '/MatchingIncomeReconsulationReport' || this.currentRoute == '/RenewalIncomeReconciliationReport' || this.currentRoute == '/WelcomeLetterDetails' || this.currentRoute == '/Customerinfo' || this.currentRoute == '/OrderHistory'
+          || this.currentRoute == '/CustomerInvoices' || this.currentRoute == '/CustomerKYCDocument' || this.currentRoute == '/download' || this.currentRoute == '/customerChangepassword' || this.currentRoute == '/RenewalDetails' || this.currentRoute == '/ShowIdCardDetails'
+          || this.currentRoute == '/UpgradeDetails') {
+          this.set = false;
+        }
+      }
+
+
+    });
+  }
 
   ngOnInit(): void {
   }
