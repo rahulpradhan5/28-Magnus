867ce498c60cc3154bf94d9660fe4a211bec8e0b
rahulpradhan5 <prahulkumar591@gmail.com>
2023-03-03 19:52:00 +0530

    api and data fetching

--- src/app/dashboard-routes/dashboard-routes.module.ts
+++ src/app/dashboard-routes/dashboard-routes.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FixedTemplateComponent } from './fixed-template/fixed-template.component';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; 
 
 const routes: Routes = [
    {path: 'vaibhav', component: FixedTemplateComponent},
@@ -12,7 +14,9 @@ const routes: Routes = [
     FixedTemplateComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    HttpClientModule // Add HttpClientModule to the imports array
   ]
 })
 export class DashboardRoutesModule { }
